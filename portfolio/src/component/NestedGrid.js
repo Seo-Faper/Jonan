4430f2bdaf0eb62def9c3398b77cc0242f56f4c3
seo-faper <dswhdcks@gmail.com>
2023-02-22 04:29:02 +0900

    OvErPoWer

--- portfolio/src/component/NestedGrid.js
+++ portfolio/src/component/NestedGrid.js
@@ -10,64 +10,76 @@ import json_data from "../res/heroTier.json";
 import RaderChart from "./RadarChart";
 import Badge from "react-bootstrap/Badge";
 import Container from "react-bootstrap/esm/Container";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+import Typography from "@mui/material/Typography";
+
 function MyVerticallyCenteredModal(props) {
   return (
-    <Modal {...props} size="lg">
-      <Modal.Header closeButton>
-        <h4>
-          백발백중 히카리 <Badge bg="danger">★OP</Badge>{" "}
-        </h4>
-      </Modal.Header>
-      <Modal.Body>
-        <Box sx={{ flexGrow: 1 }}>
-          <Grid container spacing={1} sx={{ justifyContent: "center" }}>
-            <div style={{ display: "flex" }}>
-              <Item>
-                <div>
-                <img
-                  src="https://raw.githubusercontent.com/cq-pandora/assets/master/portraits/portrait_hikari_default.png"
-                  width="250px"
-                ></img>
-
-                </div>
-
-              </Item>
-              <Item>
-                <RaderChart data={json_data.data}></RaderChart>
-              </Item>
-            </div>
-          </Grid>
-        </Box>
-        <hr></hr>
-        <Container>
-          <Grid container spacing={1}>
-          <Item>
-            <div style={{display:"flex"}}>
-
-             <img src="https://raw.githubusercontent.com/cq-pandora/assets/master/weapons/bo_s6_07.png" width={"100"}></img> 
-                <div style={{display:"grid"}}>
-                  <img src="https://raw.githubusercontent.com/cq-pandora/assets/master/sigils/ui_item_stone_5_set_13.png"></img>
-                  <img src="https://raw.githubusercontent.com/cq-pandora/assets/master/sigils/ui_item_stone_5_unique_28.png"></img>
+    <ThemeProvider theme={theme}>
+      <Modal {...props} size="lg">
+        <Modal.Header closeButton>
+          <h4>
+            백발백중 히카리 <Badge bg="danger">★OP</Badge>{" "}
+          </h4>
+        </Modal.Header>
+        <Modal.Body>
+          <Box sx={{ flexGrow: 1 }}>
+            <Grid container spacing={1} sx={{ justifyContent: "center" }}>
+              <div style={{ display: "flex" }}>
+                <Item>
+                  <div>
+                    <img
+                      src="https://raw.githubusercontent.com/cq-pandora/assets/master/portraits/portrait_hikari_default.png"
+                      width="250px"
+                    ></img>
                   </div>
-                <div style={{display:"grid"}}>
-                  <img src="https://raw.githubusercontent.com/cq-pandora/assets/master/rings/new_ring_crit_dmg_6.png"></img>
-                  <img src="https://raw.githubusercontent.com/cq-pandora/assets/master/skills/ui_skill_icon_archer_fastshot_spirit.png" width={"45"}></img>
+                </Item>
+                <Item>
+                  <RaderChart data={json_data.data}></RaderChart>
+                </Item>
+              </div>
+            </Grid>
+          </Box>
+          <hr></hr>
+          <Container>
+            <Grid container spacing={1}>
+              <Item>
+                <div style={{ display: "flex" }}>
+                  <img
+                    src="https://raw.githubusercontent.com/cq-pandora/assets/master/weapons/bo_s6_07.png"
+                    width={"100"}
+                  ></img>
+                  <div style={{ display: "grid" }}>
+                    <img src="https://raw.githubusercontent.com/cq-pandora/assets/master/sigils/ui_item_stone_5_set_13.png"></img>
+                    <img src="https://raw.githubusercontent.com/cq-pandora/assets/master/sigils/ui_item_stone_5_unique_28.png"></img>
+                  </div>
+                  <div style={{ display: "grid" }}>
+                    <img src="https://raw.githubusercontent.com/cq-pandora/assets/master/rings/new_ring_crit_dmg_6.png"></img>
+                    <img
+                      src="https://raw.githubusercontent.com/cq-pandora/assets/master/skills/ui_skill_icon_archer_fastshot_spirit.png"
+                      width={"45"}
+                    ></img>
+                  </div>
                 </div>
-                              
-            </div>
-          </Item>
-          <Item>
-            <h3>데미지 타입</h3>
-          <h4><Badge bg="primary">마법</Badge>{" "}</h4>
-          </Item>
-          </Grid>
-        
-        </Container>
-      </Modal.Body>
-      <Modal.Footer>
-        <Button onClick={props.onHide}>닫기</Button>
-      </Modal.Footer>
-    </Modal>
+              </Item>
+              <Item>
+                <Typography variant="h6">데미지 타입 / 메커니즘</Typography>
+                <h4>
+                  <Badge bg="primary">마법</Badge>{" "}
+                  <Badge bg="primary">평타 기반</Badge>{" "}
+                </h4>
+                <h4>
+                  <Badge bg="danger">적 넉백</Badge>{" "}
+                </h4>
+              </Item>
+            </Grid>
+          </Container>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button onClick={props.onHide}>닫기</Button>
+        </Modal.Footer>
+      </Modal>
+    </ThemeProvider>
   );
 }
 const Item = styled(Paper)(({ theme }) => ({
@@ -87,7 +99,19 @@ const Port = styled(Paper)(({ theme }) => ({
   width: 100,
   color: theme.palette.text.secondary,
 }));
+const theme = createTheme();
 
+theme.typography.h6 = {
+  fontSize: "1.2rem",
+  "@media (min-width:600px)": {
+    fontSize: "1.2rem",
+    color: "#000000",
+  },
+  [theme.breakpoints.up("md")]: {
+    fontSize: "1.2rem",
+    color: "#000000",
+  },
+};
 export default function ResponsiveGrid() {
   const [modalShow, setModalShow] = React.useState(false);
   return (
--- portfolio/src/component/RadarChart.js
+++ portfolio/src/component/RadarChart.js
@@ -1,62 +1,76 @@
 // install (please make sure versions match peerDependencies)
 // yarn add @nivo/core @nivo/radar
-import * as React from 'react';
-import { ResponsiveRadar } from '@nivo/radar'
-
-
+import * as React from "react";
+import { ResponsiveRadar } from "@nivo/radar";
+import "../css/tier.css";
 // make sure parent container have a defined height when using
 // responsive component, otherwise height will be 0 and
 // no chart will be rendered.
 // website examples showcase many properties,
 // you'll often use just a few of them.
 
+const tooltip = (props) => {
+  // console.log(props.data[0])
+  const rank = { 1: "F", 2: "B", 3: "A", 4: "S", 5: "OverPower" };
+  const colors = {
+    F: "gray",
+    B: "green",
+    A: "red",
+    S: "orange",
+    OverPower: "",
+  };
+  let tier = props.data[0].value;
+  let color = colors[rank[tier]];
 
-const tooltip = (props)=>{
-   // console.log(props.data[0])
-   const rank = {1:'F',2:'B',3:'A',4:'S',5:'OP'}
-   const colors = {'F':'gray','B':'green','A':'red','S':'orange','OP':'black'}
-    let tier = props.data[0].value;
-    let color = colors[rank[tier]];
-    return <div>
-        <b><h5>{props.index}</h5> <h4 style={{color:color, fontWeight:900}}>{rank[tier]}</h4></b>
+  return (
+    <div>
+      <b>
+        <h5>{props.index}</h5>{" "}
+        <h4
+          className={rank[[tier]] === "OverPower" ? "OverPower" : ""}
+          style={{ color: color, fontWeight: 900 }}
+        >
+          {rank[tier]}
+        </h4>
+      </b>
     </div>
-}
+  );
+};
 const RadarChart = (e) => {
-
-    return(
-        <div style={{ width: '500px', height: '400px', margin: '0 auto'  ,
-        justifyContent: 'center',
-        borderColor: 'black'
-        }}>
-    <ResponsiveRadar
+  return (
+    <div
+      style={{
+        width: "500px",
+        height: "400px",
+        margin: "0 auto",
+        justifyContent: "center",
+        borderColor: "black",
+      }}
+    >
+      <ResponsiveRadar
         data={e.data}
-        keys={['티어']}
+        keys={["티어"]}
         indexBy="컨텐츠"
         maxValue={5}
         margin={{ top: 70, right: 80, bottom: 40, left: 80 }}
-
-        borderColor={{ from: 'color' }}
+        borderColor={{ from: "color" }}
         sliceTooltip={tooltip}
         theme={{
-            
-            axis:{
-                
-                ticks:{
-                    text:{
-                        fontSize: 20,
-                        fill: '#000000' 
-                        
-                    }
-                    
-                },
-            }
+          axis: {
+            ticks: {
+              text: {
+                fontSize: 20,
+                fill: "#000000",
+              },
+            },
+          },
         }}
         gridShape="linear"
         gridLabelOffset={10}
         enableDots={false}
-        
-        colors={{ scheme: 'category10' }}
-    />
-    </div>);
+        colors={{ scheme: "category10" }}
+      />
+    </div>
+  );
 };
 export default RadarChart;
